19545fdb692ae467886113714426de4c970c9ce0
Jaid <jaid.jsx@gmail.com>
2019-07-02 15:03:27 +0200

    Added some testing code
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 /** @module steam-game-update-watcher */
 
 import PollingEmitter from "polling-emitter"
+import got from "got"
+import matchArray from "match-array"
+
+const fetchUpdatesRegex = new RegExp("data-changeid=\"(?<id>\\w+?)\"(?<changes>.+?)(<\\/ul|$)", "gs")
+const fetchChangesRegex = new RegExp("class=\"octicon octicon-(?<icon>.+?)\"", "gs")
 
 /**
  * Returns the number of seconds passed since Unix epoch (01 January 1970)
@@ -13,11 +18,52 @@ import PollingEmitter from "polling-emitter"
  */
 export default class extends PollingEmitter {
 
-  constructor() {
+  constructor(options) {
     super({
-      pollIntervalSeconds: 300,
+      pollIntervalSeconds: 30,
       invalidateInitialEntries: true,
+      fetchEntries: async () => {
+        const result = await this.got("api/GetDepotHistory", {
+          json: true,
+          query: {
+            depotid: this.options.depotId,
+          },
+        })
+        if (!result.body.success) {
+          throw new Error(`Did not work: result.body.success is ${result.body.success}`)
+        }
+        const updateBlocks = matchArray(fetchUpdatesRegex, result.body.data.Rendered)
+        for (const updateBlock of updateBlocks) {
+          updateBlock.changes = matchArray(fetchChangesRegex, updateBlock.changes).map(({icon}) => icon)
+        }
+        return updateBlocks
+      },
+      ...options,
     })
+    this.got = got.extend({
+      baseUrl: "https://steamdb.info",
+    })
+    this.on("invalidatedEntry", entry => {
+      console.log(entry)
+      debugger
+    })
+  }
+
+  async fetchEntries() {
+    const result = await this.got("api/GetDepotHistory", {
+      json: true,
+      query: {
+        depotid: this.options.depotId,
+      },
+    })
+    if (!result.body.success) {
+      throw new Error(`Did not work: result.body.success is ${result.body.success}`)
+    }
+    const updateBlocks = matchArray(fetchUpdatesRegex, result.body.data.Rendered)
+    for (const updateBlock of updateBlocks) {
+      updateBlock.changes = matchArray(fetchChangesRegex, updateBlock.changes).map(({icon}) => icon)
+    }
+    return updateBlocks
   }
 
 }
--- test/test.js
+++ test/test.js
@@ -1,13 +1,15 @@
 import path from "path"
 
+import delay from "delay"
+
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
 
 /**
    * @type { import("../src") }
    */
-const {default: steamGameUpdateWatcher} = indexModule
+const {default: SteamGameUpdateWatcher} = indexModule
 
-it("should run", () => {
-  const result = steamGameUpdateWatcher()
-  expect(result).toBeGreaterThan(1549410770)
+it("should run", async () => {
+  const watcher = new SteamGameUpdateWatcher({depotId: "381211"})
+  delay(60000)
 })
