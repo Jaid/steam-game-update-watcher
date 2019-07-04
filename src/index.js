feb71ae609879e41f26f24500be771954ece47b7
Jaid <jaid.jsx@gmail.com>
2019-07-04 08:58:56 +0200

    Migrated to new `polling-emitter`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -8,36 +8,15 @@ const fetchUpdatesRegex = new RegExp("data-changeid=\"(?<id>\\w+?)\"(?<changes>.
 const fetchChangesRegex = new RegExp("class=\"octicon octicon-(?<icon>.+?)\"", "gs")
 
 /**
- * Returns the number of seconds passed since Unix epoch (01 January 1970)
- * @example
- * import steamGameUpdateWatcher from "steam-game-update-watcher"
- * const result = steamGameUpdateWatcher()
- * result === 1549410770
- * @function
- * @returns {number} Seconds since epoch
- */
+ * @class
+ *  */
 export default class extends PollingEmitter {
 
   constructor(options) {
     super({
-      pollIntervalSeconds: 30,
+      pollIntervalSeconds: 10,
       invalidateInitialEntries: true,
-      fetchEntries: async () => {
-        const result = await this.got("api/GetDepotHistory", {
-          json: true,
-          query: {
-            depotid: this.options.depotId,
-          },
-        })
-        if (!result.body.success) {
-          throw new Error(`Did not work: result.body.success is ${result.body.success}`)
-        }
-        const updateBlocks = matchArray(fetchUpdatesRegex, result.body.data.Rendered)
-        for (const updateBlock of updateBlocks) {
-          updateBlock.changes = matchArray(fetchChangesRegex, updateBlock.changes).map(({icon}) => icon)
-        }
-        return updateBlocks
-      },
+      autostart: true,
       ...options,
     })
     this.got = got.extend({
--- test/test.js
+++ test/test.js
@@ -1,15 +1,16 @@
 import path from "path"
 
 import delay from "delay"
+import ms from "ms.macro"
 
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
 
 /**
-   * @type { import("../src") }
-   */
+  * @type { import("../src") }
+*/
 const {default: SteamGameUpdateWatcher} = indexModule
 
 it("should run", async () => {
   const watcher = new SteamGameUpdateWatcher({depotId: "381211"})
-  delay(60000)
-})
+  await delay(ms`15 seconds`)
+}, ms`1 minute`)
